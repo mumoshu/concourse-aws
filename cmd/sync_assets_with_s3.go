24b550a8b93a8112bae80c75bd2834a76f05be8b
Shingo Omura <everpeace@gmail.com>
2016-08-09 18:14:46 +0900

    review: delete unnecessary comments generated by cobra.
    ref:
    https://github.com/mumoshu/concourse-aws/pull/9#discussion_r74021376

--- cmd/sync_assets_with_s3.go
+++ cmd/sync_assets_with_s3.go
@@ -79,16 +79,6 @@ func RunGetStatesFromS3(cmd *cobra.Command, args []string) {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// putStatesToS3.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// putStatesToS3.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	RootCmd.AddCommand(putStatesToS3)
 	putStatesToS3.Flags().StringVar(&bucketName, "bucket", "", "S3 bucket name to which assets will be uploaded.")
 	putStatesToS3.Flags().StringVar(&bucketRegion, "bucket-region", "", "Region of S3 Bucket specified by --bucket")
